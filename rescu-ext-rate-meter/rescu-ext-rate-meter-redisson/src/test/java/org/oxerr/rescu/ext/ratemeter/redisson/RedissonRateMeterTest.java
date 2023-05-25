473cf119d2b53ca7e8b83494c3c6f459c4231cc9
Joe Zhou <zhoushuqun@gmail.com>
2023-05-26 00:26:19 +0800

    Fix rate meter.

--- rescu-ext-rate-meter/rescu-ext-rate-meter-redisson/src/test/java/org/oxerr/rescu/ext/ratemeter/redisson/RedissonRateMeterTest.java
+++ rescu-ext-rate-meter/rescu-ext-rate-meter-redisson/src/test/java/org/oxerr/rescu/ext/ratemeter/redisson/RedissonRateMeterTest.java
@@ -8,6 +8,8 @@ import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalUnit;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
 
 class RedissonRateMeterTest {
 
@@ -18,8 +20,31 @@ class RedissonRateMeterTest {
 
 		TemporalUnit unit = ChronoUnit.SECONDS;
 
-		long rate = RedissonRateMeter.getRate(600, s, e, unit);
+		long rate = RedissonRateMeter.getRate(600, s, e, unit).get().longValue();
 		assertEquals(10, rate);
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		final RedissonClient redisson = Redisson.create();
+		final String keyPrefix = "test";
+
+		final RedissonRateMeter meter = new RedissonRateMeter(redisson, keyPrefix);
+
+		for (int i = 0; i < 50; i++) {
+			meter.increment();
+		}
+
+		System.out.println("Sleeping 5 seconds...");
+		Thread.sleep(5000);
+
+		final long rate = meter.getRate(ChronoUnit.SECONDS).get().longValue();
+		System.out.printf("rate: %d\n", rate);
+
+		System.out.println("Sleeping 15 seconds...");
+		Thread.sleep(15 * 1000);
+
+		System.out.println("Shutting down...");
+		redisson.shutdown();
 	}
 
 }
