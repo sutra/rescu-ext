5e620c5af4151fd5c48ba374489af1f75204f720
Joe Zhou <zhoushuqun@gmail.com>
2025-02-09 03:16:57 +0800

    Use new method to set rate.

--- rescu-ext-rate-limiter/rescu-ext-rate-limiter-redisson/src/main/java/org/oxerr/rescu/ext/ratelimiter/redisson/RedissonRateLimiter.java
+++ rescu-ext-rate-limiter/rescu-ext-rate-limiter-redisson/src/main/java/org/oxerr/rescu/ext/ratelimiter/redisson/RedissonRateLimiter.java
@@ -1,5 +1,7 @@
 package org.oxerr.rescu.ext.ratelimiter.redisson;
 
+import java.time.Duration;
+
 import org.oxerr.rescu.ext.ratelimiter.RateLimiter;
 import org.redisson.api.RRateLimiter;
 import org.redisson.api.RateIntervalUnit;
@@ -21,13 +23,27 @@ public class RedissonRateLimiter implements RateLimiter {
 	/**
 	 * Updates RateLimiter's state and stores config to Redis server.
 	 *
-	 * @param mode             - rate mode
-	 * @param rate             - rate
-	 * @param rateInterval     - rate time interval
+	 * @param mode			 - rate mode
+	 * @param rate			 - rate
+	 * @param rateInterval	 - rate time interval
 	 * @param rateIntervalUnit - rate time interval unit
+	 * @deprecated Use {@link #setRate(RateType, long, Duration)} instead.
 	 */
+	@Deprecated(forRemoval = true, since = "1.1.0")
 	public void setRate(RateType mode, long rate, long rateInterval, RateIntervalUnit rateIntervalUnit) {
 		this.rateLimiter.setRate(mode, rate, rateInterval, rateIntervalUnit);
+	}
+
+	/**
+	 * Sets the rate limit and clears the state.
+	 * Overrides both limit and state if they haven't been set before.
+	 *
+	 * @param mode rate mode
+	 * @param rate rate
+	 * @param rateInterval rate time interval
+	 */
+	public void setRate(RateType mode, long rate, Duration rateInterval) {
+		this.rateLimiter.setRate(mode, rate, rateInterval);
 	}
 
 	@Override
